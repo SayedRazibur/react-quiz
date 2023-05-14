7a284a184e3cf941c5e7584261bf3b0b8c80b2e4
Sayed Razibur Rahman <114426159+SayedRazibur@users.noreply.github.com>
2023-05-15 01:48:14 +0600

    vjv

---
+++ src/components/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const Home = () => {
+  return (
+    <div>
+      Home Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+      distinctio omnis cum quo facilis hic, provident voluptates, ipsum
+      assumenda, ea quis earum dolorum maxime iusto. Praesentium nisi saepe
+      itaque vitae, dolor cum. Eius minus officiis nobis reprehenderit totam.
+      Porro fugit dicta ea repudiandae quia soluta consequuntur, cupiditate
+      culpa qui perspiciatis doloremque quaerat accusantium ab! Atque qui nihil
+      minus autem ipsa facere illum necessitatibus expedita? Labore facere et
+      commodi adipisci, minima molestiae officiis iure quo quae nesciunt
+      architecto odio similique nostrum minus itaque fugit magni, omnis quidem
+      porro possimus hic quod, nemo exercitationem saepe! Earum, repudiandae
+      sint suscipit nesciunt libero quos. hello
+    </div>
+  );
+};
